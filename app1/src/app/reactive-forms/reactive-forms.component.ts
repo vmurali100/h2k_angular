61038abaa0b1f920f3fb5e62e1d25728d384b990
vmurali100 <vmurali100@gmail.com>
2020-02-07 08:01:27 +0530

    Adding Some

--- app1/src/app/reactive-forms/reactive-forms.component.ts
+++ app1/src/app/reactive-forms/reactive-forms.component.ts
@@ -9,6 +9,13 @@ import { FormGroup, FormControl } from "@angular/forms";
 export class ReactiveFormsComponent implements OnInit {
   userForm: FormGroup;
   myUsers = [];
+
+  editUser = {
+    email: "",
+    password: ""
+  };
+  isEdit: boolean = false;
+  index: any;
   constructor() {}
 
   ngOnInit() {
@@ -22,6 +29,18 @@ export class ReactiveFormsComponent implements OnInit {
   handleSubmit() {
     this.myUsers = JSON.parse(localStorage.getItem("users"));
     this.myUsers.push(this.userForm.value);
+    localStorage.setItem("users", JSON.stringify(this.myUsers));
+    this.userForm.reset();
+  }
+
+  editUserParent(userInfo) {
+    this.editUser = Object.assign({}, userInfo.user);
+    this.isEdit = true;
+    this.index = userInfo.i;
+  }
+
+  updateUser() {
+    this.myUsers[this.index] = this.editUser;
     localStorage.setItem("users", JSON.stringify(this.myUsers));
     this.userForm.reset();
   }
--- app1/src/app/user-list/user-list.component.ts
+++ app1/src/app/user-list/user-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
   selector: "app-user-list",
@@ -7,7 +7,17 @@ import { Component, OnInit, Input } from "@angular/core";
 })
 export class UserListComponent implements OnInit {
   @Input() users: any;
+  @Output() sendUser = new EventEmitter();
   constructor() {}
 
   ngOnInit() {}
+
+  editUser(user, i) {
+    console.log(i);
+    let userInfo = {
+      user,
+      i
+    };
+    this.sendUser.emit(userInfo);
+  }
 }
